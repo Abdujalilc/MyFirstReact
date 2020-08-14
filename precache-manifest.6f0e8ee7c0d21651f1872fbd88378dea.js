43f8614a12416f63ea7d7b7a54292e3731bf640a
Abdujalilc <abdujalilc@gmail.com>
2020-08-14 22:09:33 +0500

    Updates

---
+++ precache-manifest.6f0e8ee7c0d21651f1872fbd88378dea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8ad3b3227cabb984d60d5a436cd141",
+    "url": "/MyFirstReact/index.html"
+  },
+  {
+    "revision": "af71f6874ffa20ba1103",
+    "url": "/MyFirstReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/MyFirstReact/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyFirstReact/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af71f6874ffa20ba1103",
+    "url": "/MyFirstReact/static/js/main.363b5642.chunk.js"
+  },
+  {
+    "revision": "989824971ea2f1e05ba0",
+    "url": "/MyFirstReact/static/js/runtime-main.b43b23d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyFirstReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
